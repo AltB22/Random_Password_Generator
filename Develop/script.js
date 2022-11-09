9e820f98947270498da535b2ad7a33be07e96448
Alt B 2022 <massie.w@gmail.com>
2022-11-09 11:21:24 -0800

    This commit is to catch up what I did last night till 1:30amgit add .  Forgot to commit..Created 4 variables for each type of character.  Created function to generate password.  Including qualifying character count and number type with if statements.  Creatred 4 more vars to capture confirms regarding each type of character.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numVal = "1234567890"
+var specialChar = "!@#$%^&*()"
 
-"!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+// console.log(upperCase.split(""))
+
+function generatePassword() {
+  
+  var charCount = prompt("Please enter number of characters 8-128")
+
+  if (charCount < 8 || charCount > 128) {
+    alert("Invalid # of characters, please try again")
+    generatePassword()
+  }
+
+  if (isNaN(charCount)) {
+    alert("Please enter a valid number")
+    generatePassword()
+  }
+
+
+  var includeNumber = confirm("Include Numbers?");
+  var includeUpper = confirm("Include Uppercase?");
+  var includeLower = confirm("Include Lowercase?");
+  var includeSpecial = confirm("Include Special Characters");
+
+  var selectedCharType = []
+
+  if (includeNumber) {
+    selectedCharType.push(numVal)
+    //console.log(selectedCharType)
+  }
+  if (includeUpper) {
+    selectedCharType.push(upperCase)
+    //console.log(selectedCharType)
+  }
+  if (includeLower) {
+    selectedCharType.push(lowerCase)
+    //console.log(selectedCharType)
+  }
+  if (includeSpecial) {
+    selectedCharType.push(specialChar)
+    //console.log(selectedCharType)
+  }
+  if (!includeNumber && !includeUpper && !includeLower && !includeSpecial) {
+    alert("Please select at least one character type")
+  }
+}
+  //Need to concat the strings selected into single string
+
+  //Then create for loop to generate output
+
+
 
 
 
@@ -16,3 +68,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// while (charCount=null){
+//   generatePassword()
+// }
